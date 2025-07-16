8d5823968dd34c001f3e4ee6498b94398712d0a3
Erik <erikjbarra@gmail.com>
2025-07-15 21:35:40 -0400

    T2 Lista

---
+++ emotion_detection.py
@@ -0,0 +1,18 @@
+import json
+import requests
+
+def emotion_detector(text_to_analyze):
+
+    url = (
+        "https://sn-watson-emotion.labs.skills.network/v1/"
+        "watson.runtime.nlp.v1/NlpService/EmotionPredict"
+    )
+
+    text_to_analyze = {"raw_document": {"text": text_to_analyze}}
+    headers = {
+        "grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"
+    }
+
+    response = requests.post(url, json=text_to_analyze, headers=headers)
+
+    return response.text
